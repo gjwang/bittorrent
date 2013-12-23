c5da2d7820db7e49f840152f1d6ac547a933c622
gjwang <guijiewan@gmail.com>
2013-12-22 18:41:33 -0800

    add help info to bttracker.sh

--- BitTorrent-4.4.0/bttracker.sh
+++ BitTorrent-4.4.0/bttracker.sh
@@ -1,1 +1,41 @@
-python BitTorrent-4.4.0/bittorrent-tracker.py --port 8090 --dfile downloaders.log --reannounce_interval 60 &
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides:          gjwang
+# Required-Start:    $local_fs $remote_fs $network $syslog $named
+# Required-Stop:     $local_fs $remote_fs $network $syslog $named
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: start/stop/restart the bt tracker server
+# Description:       starts bt tracker server using start-stop-daemon
+### END INIT INFO
+
+
+startup()
+{
+    python bittorrent-tracker.py --port 8090 --dfile downloaders.log --reannounce_interval 60 &
+}
+
+shutdown()
+{
+	ps -ef|grep "bittorrent-tracker.py"|grep -v grep|awk '{print $2}'|xargs kill
+}
+
+case "$1" in
+        start)
+                startup
+                ;;
+        stop)
+                shutdown
+                ;;
+        restart)
+                shutdown
+                startup
+                ;;
+
+        *)
+                echo "usage: start | stop | restart"
+                exit 1
+                ;;
+esac
+exit
